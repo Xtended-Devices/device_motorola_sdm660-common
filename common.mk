20cbe9945ac764248f7c32233325bf2d98e7a85c
cool585 <dhineshcool585@gmail.com>
2021-01-13 07:24:17 +0530

    Revert "sdm660-common: Use CodeAurora ImsService implementation for RCS"
    This reverts commit 5e2a0b23b66d942215ed12c992cef83745026043.

--- common.mk
+++ common.mk
@@ -337,6 +337,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # Radio
 PRODUCT_PACKAGES += \
     librmnetctl \
